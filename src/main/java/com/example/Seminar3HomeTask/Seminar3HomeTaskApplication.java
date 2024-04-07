3c7b2fb01591d977ac743eea70a31d1d49d8e09e
Roman Turusov <turusov97@yandex.ru>
2024-04-07 21:05:05 +0300

    Initial commit

---
+++ src/main/java/com/example/Seminar3HomeTask/Seminar3HomeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.Seminar3HomeTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar3HomeTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminar3HomeTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Seminar3HomeTask/Seminar3HomeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Seminar3HomeTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar3HomeTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
